5c30e3277a4f270c24cc0a540c5c8ba52d062e75
Fabrizio Brolis <Fabrizio Brolis>
2016-09-02 18:36:35 +0200

    Riconfigurazione indirizzi

--- DromegaWebConf/DromegaWebConf.h
+++ DromegaWebConf/DromegaWebConf.h
@@ -22,14 +22,14 @@ uint8_t ip_gateway_Router[4] = {192, 168, 2, ip_Router};// indirizzo Gateway rou
 #define	myvNet_supern_E2	Nodo02_Bridge01_RS485
 #define	myvNet_supern_E3	Nodo03_Bridge02_RS485
 
-#define Nodo02_Bridge01_RS485		    0xCE01	// Indirizzo Nodo02 bridge01 della Seriale RS485
-#define Nodo04_Bridge01_Peer01			0xCE02	// Indirizzo Nodo04 peer01 su bridge01 remota Seriale RS485
-#define Nodo05_Bridge01_Peer02			0xCE03	// Indirizzo Nodo05 peer02 su bridge01 remota Seriale RS485
+#define Nodo02_Bridge01_RS485		    0xCE00	// Indirizzo Nodo02 bridge01 della Seriale RS485
+#define Nodo04_Bridge01_Peer01			0xCE01	// Indirizzo Nodo04 peer01 su bridge01 remota Seriale RS485
+#define Nodo05_Bridge01_Peer02			0xCE02	// Indirizzo Nodo05 peer02 su bridge01 remota Seriale RS485
 
-#define Nodo03_Bridge02_RS485		    0xCF01	// Indirizzo Nodo03 bridge02 della Seriale RS485
-#define Nodo06_Bridge02_Peer01			0xCF02	// Indirizzo Nodo06 peer01 su bridge02 remota Seriale RS485
-#define Nodo07_Bridge02_Peer02			0xCF03	// Indirizzo Nodo07 peer02 su bridge02 remota Seriale RS485
-#define Nodo08_Bridge02_Peer03			0xCF04	// Indirizzo Nodo08 peer03 su bridge02 remota Seriale RS485
+#define Nodo03_Bridge02_RS485		    0xCF00	// Indirizzo Nodo03 bridge02 della Seriale RS485
+#define Nodo06_Bridge02_Peer01			0xCF01	// Indirizzo Nodo06 peer01 su bridge02 remota Seriale RS485
+#define Nodo07_Bridge02_Peer02			0xCF02	// Indirizzo Nodo07 peer02 su bridge02 remota Seriale RS485
+#define Nodo08_Bridge02_Peer03			0xCF03	// Indirizzo Nodo08 peer03 su bridge02 remota Seriale RS485
 
 // -------------FINE definizione della configurazione di rete --DromegaWeb------------------------------------
 
